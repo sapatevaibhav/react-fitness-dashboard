6191252809b603cd0cbeb397feb4ac02b5ab55e6
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-16 01:00:07 +0530

    made header better

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 
 function Header() {
+  const [currentDate, setCurrentDate] = useState('');
+  const [currentTime, setCurrentTime] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const date = now.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
+      const time = now.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+      setCurrentDate(date);
+      setCurrentTime(time);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <header className="header">
       <div className="greeting">
@@ -9,14 +24,18 @@ function Header() {
         <p>Howz the josh?</p>
       </div>
       <div className="search-bar">
-        <input type="text" placeholder="Search" />
-        <button><i className="fa fa-search"></i></button>
+        <div className="search-input-wrapper">
+          <input type="text" placeholder="Search" />
+          <img src="/assets/search.svg" alt="Search" className="search-icon" />
+        </div>
       </div>
-      <div className="date-time">
-        <span>Jun 10, 2024</span>
-        <span>9:41 AM</span>
+      <div className="date-time noti">
+        <span>{currentDate}</span> <span>{currentTime}</span>
+        <img src="/assets/notification.svg" alt="notification" className="noti-image" />
       </div>
-      <img src="/assets/user.png" alt="User" className="user-image" />
+      <div className="user-profile">
+        <img src="/assets/user.png" alt="User" className="user-image" />
+      </div>
     </header>
   );
 }
