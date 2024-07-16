301eeaebdc37de7496cdb600f441bf7e9ad6de68
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-16 23:33:28 +0530

    small change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Sidebar from './Sidebar';
-import ProgressChart from './ProgressChart';
-import GoalsChart from './GoalsChart';
-import Header from './Header';
-import './Home.css';
+import React from "react";
+import Sidebar from "./Sidebar";
+import ProgressChart from "./ProgressChart";
+import GoalsChart from "./GoalsChart";
+import Header from "./Header";
+import "./Home.css";
 
 function Home() {
   return (
@@ -13,13 +13,12 @@ function Home() {
         <Header />
         <div className="charts">
           <div className="progress-chart">
-            <h2>Progress</h2>
+            <h2 className="header-2">Progress</h2>
             <div className="main-chart">
               <ProgressChart />
             </div>
           </div>
           <div className="goals-chart">
-            <h2>Goal</h2>
             <GoalsChart />
           </div>
         </div>
