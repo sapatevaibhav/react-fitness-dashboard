0fcff13989bf1a880947f3a2811def86a253a9bd
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-16 23:32:33 +0530

    improved goals

--- src/components/GoalsChart.jsx
+++ src/components/GoalsChart.jsx
@@ -1,39 +1,53 @@
-import React from 'react';
-import './GoalsChart.css';
+import React from "react";
+import "./GoalsChart.css";
 
 function GoalsChart() {
   return (
-    <div className="goals">
-      <div className="goal-item">
-        <span>Sleep at night</span>
-        <div className="progress-bar">
-          <div className="progress" style={{ width: '90%' }}></div>
-        </div>
-        <span>20 nights in row</span>
+    <div className="goal-chart">
+      <div className="goals-header">
+        <h2>Goal</h2>
+        <button className="set-goal-button">set new fitness goal</button>
       </div>
       <div className="goal-item">
-        <span>Workout 5/7</span>
-        <div className="progress-bar">
-          <div className="progress" style={{ width: '60%' }}></div>
+        <div className="text-container" align="center">
+          <span className="main-text">Sleep at night</span>
+          <span className="sub-text">progress</span>
+          <span className="sub-text">20 nights in row</span>
         </div>
-        <span>2 weeks in row</span>
+        <div className="progress-bar">
+          <div className="progress" style={{ width: "30%" }}></div>
+        </div>
       </div>
       <div className="goal-item">
-        <span>Yoga</span>
-        <div className="progress-bar">
-          <div className="progress" style={{ width: '30%' }}></div>
+        <div className="text-container" align="center">
+          <span className="main-text">Workout 5/7</span>
+          <span className="sub-text">progress</span>
+          <span className="sub-text">2 weeks in row</span>
         </div>
-        <span>
-          not done in this week</span>
+        <div className="progress-bar">
+          <div className="progress" style={{ width: "60%" }}></div>
+        </div>
       </div>
       <div className="goal-item">
-        <span>Swimming</span>
-        <div className="progress-bar">
-          <div className="progress" style={{ width: '100%' }}></div>
+        <div className="text-container" align="center">
+          <span className="main-text">Yoga</span>
+          <span className="sub-text">progress</span>
+          <span className="sub-text">not done in this week</span>
         </div>
-        <span>done</span>
+        <div className="progress-bar">
+          <div className="progress" style={{ width: "30%" }}></div>
+        </div>
       </div>
-      <button className="set-goal-button">set new fitness goal</button>
+      <div className="goal-item">
+        <div className="text-container" align="center">
+          <span className="main-text">Swimming</span>
+          <span className="sub-text">progress</span>
+          <span className="sub-text">done</span>
+        </div>
+        <div className="progress-bar">
+          <div className="progress" style={{ width: "100%" }}></div>
+        </div>
+      </div>
     </div>
   );
 }
