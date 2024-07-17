543d2e2dc0ad8ce7bd34d668f01a0bbf37b4eeb4
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-17 21:53:56 +0530

    added dashboard page

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import TotalWorkoutChart from "./TotalWorkoutChart";
+import CaloriesBurnedChart from "./CaloriesBurnedChart";
+import HeartRateChart from "./HeartRateChart";
+import FitnessGoalsChart from "./FitnessGoalsChart";
+import Calendar from "react-calendar";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "react-calendar/dist/Calendar.css";
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <div className="chart-wrapper">
+        <div className="top-charts">
+          <TotalWorkoutChart />
+          <CaloriesBurnedChart />
+          <HeartRateChart />
+        </div>
+      </div>
+      <div className="chart-wrapper">
+        <div className="middle-charts">
+          <FitnessGoalsChart />
+          <div className="calendar-wrapper">
+            <Calendar />
+          </div>
+        </div>
+      </div>
+      <div className="chart-wrapper">
+        <div className="bottom-map">
+          <MapContainer
+            center={[18.5204, 73.8567]}
+            zoom={13}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={[18.5204, 73.8567]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
