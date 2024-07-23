8d2e3d477726de3a9e8b24b085dd63a42830778f
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-24 00:48:58 +0530

    seperated profile card

---
+++ src/components/Profile/ProfileCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ProfileCard = () => {
+  return (
+    <div className="profile-card">
+      <img src="/assets/user.png" alt="Profile" className="profile-img" />
+      <h2 className="profile-name">amit barve</h2>
+      <p className="profile-level">beginner</p>
+      <button className="edit-profile-btn">edit profile</button>
+      <div className="profile-buttons">
+        <button className="profile-btn">credits</button>
+        <button className="profile-btn">fitness level</button>
+      </div>
+    </div>
+  );
+};
+export default ProfileCard;
