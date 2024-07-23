e91f5f81fdf1798a59122c6da6ae58483b0a7613
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-23 21:28:31 +0530

    changes to match design

--- src/components/workouts/WorkoutType.jsx
+++ src/components/workouts/WorkoutType.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./Workouts.css";
 
 const workoutTypes = [
-  { type: "cardio", description: "for good heart health" },
-  { type: "endurance", description: "for strength training" },
-  { type: "weightloss", description: "for look good" },
-  { type: "muscle train", description: "target specific muscle" },
-  { type: "Menu Label", description: "Menu description." },
+  { type: "cardio", description: "for good heart health", result: "up" },
+  { type: "endurance", description: "for strength training", result: "up" },
+  { type: "weightloss", description: "for look good", result: "down" },
+  { type: "muscle train", description: "target specific muscle", result: "up" },
+  { type: "Menu Label", description: "Menu description.", result: "up" },
 ];
 
 const WorkoutType = () => {
@@ -16,8 +16,17 @@ const WorkoutType = () => {
       <ul>
         {workoutTypes.map((workout, index) => (
           <li key={index}>
-            <span className="workout-name">{workout.type}</span>
-            <span className="workout-description">{workout.description}</span>
+            <i className="fa-solid fa-star"></i>
+            <div className="workout-info">
+              <span className="workout-name">{workout.type}</span>
+              <div className="workout-description">{workout.description}</div>
+            </div>
+            {workout.result === "up" ? (
+              <i className="fa fa-up-long arrow"></i>
+            ) : (
+              <i className="fa fa-down-long arrow"></i>
+            )}
+            {workout.result === "up" ? <div>A</div> : <div>B</div>}
           </li>
         ))}
       </ul>
